852f62635ea990a5601eab2283c0323937e62341
sofia marmoucha <sofiazmarm@gmail.com>
2022-08-03 09:24:32 -0400

    Initial Commit

---
+++ estee lauder project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  estee lauder project
+//
+//  Created by Sofia Marmoucha on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ estee lauder projectUITests/estee_lauder_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  estee_lauder_projectUITestsLaunchTests.swift
+//  estee lauder projectUITests
+//
+//  Created by Sofia Marmoucha on 8/3/22.
+//
+
+import XCTest
+
+class estee_lauder_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
